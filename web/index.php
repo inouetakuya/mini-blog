31f5bf5420d58d6efee9d9124f07f85677e5640e
INOUE Takuya <inouetakuya5@gmail.com>
2012-01-01 18:12:28 +0900

    パーフェクト PHP P207
    + * フロントコントローラ
    +require '../bootstrap.php';

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * フロントコントローラ
+ *
+ * User: inouetakuya
+ * Date: 12/01/01
+ * Time: 18:11
+ * To change this template use File | Settings | File Templates.
+ */
+require '../bootstrap.php';
