f95a4d61bbf458fe51995c3f6bd8ceb943cbd053
INOUE Takuya <inouetakuya5@gmail.com>
2012-01-02 05:23:06 +0900

    P211 ベース URL と PATH_INFO
    modified: core/Request.php
    
    +     * index.php が http://example.com/foo/bar/index.php にあるとき
    +     * http://example.com/foo/bar/list の「/foo/bar」の部分
    
    +     * REQUEST_URI（ホスト部分より後ろ）: /foo/bar/list
    +     * SCRIPT_NAME（フロントコントローラまでのパス）: /foo/bar/index.php
    +     * ベース URL（ホスト部分より後ろから、フロントコントローラまでの値）: /foo/bar
    +     * PATH_INFO（ベース URL より後ろの値）: /list

--- core/Request.php
+++ core/Request.php
@@ -66,4 +66,58 @@ class Request
     {
         return $_SERVER['REQUEST_URI'];
     }
+
+    /**
+     * ベース URL（ホスト部分より後ろから、フロントコントローラまでの値）
+     *
+     * index.php が http://example.com/foo/bar/index.php にあるとき
+     * http://example.com/foo/bar/list の「/foo/bar」の部分
+     *
+     * HTML 内のリンクを作成する際に利用する
+     * フロントコントローラを用いているため、1つ前のディレクトリにある
+     * ファイル（../xxx.php）のような指定ができないため、リンクには
+     * ドキュメントルート以下の絶対 URL を指定する必要がある
+     *
+     * REQUEST_URI（ホスト部分より後ろ）: /foo/bar/list
+     * SCRIPT_NAME（フロントコントローラまでのパス）: /foo/bar/index.php
+     * ベース URL（ホスト部分より後ろから、フロントコントローラまでの値）: /foo/bar
+     * PATH_INFO（ベース URL より後ろの値）: /list
+     */
+    public function getBaseUrl()
+    {
+        $script_name = $_SERVER['SCRIPT_NAME'];
+        $request_uri = $this->getRequestUri();
+
+        // フロントコントローラが URL に含まれるとき
+        if (0 === strpos($request_uri, $script_name)) {
+            return $script_name;
+
+        // フロントコントローラが省略されているとき
+        } else if (0 === strpos($request_uri, dirname($script_name))) {
+            return rtrim(dirname($script_name), '/');
+        }
+
+        return '';
+    }
+
+    /**
+     * PATH_INFO（ベース URL より後ろの値）
+     *
+     * index.php が http://example.com/foo/bar/index.php にあるとき
+     * http://example.com/foo/bar/list の「/list」の部分
+     */
+    public function getPathInfo()
+    {
+        $base_url = $this->getBaseUrl();
+        $request_uri = $this->getRequestUri();
+
+        // REQUEST_URI に含まれる GET パラメータを取り除く
+        if (false !== ($pos = strpos($request_uri, '?'))) {
+            $request_uri = substr($request_uri, 0, $pos);
+        }
+
+        $path_info = (string)substr($request_uri, strlen($base_url));
+
+        return $path_info;
+    }
 }
