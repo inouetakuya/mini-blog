8f7eeb85af2c9630f2c669f3be7ee0fa260afd56
INOUE Takuya <inouetakuya5@gmail.com>
2012-01-10 16:52:09 +0900

    P279 register アクション

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -16,64 +16,66 @@ class AccountController extends Controller
         }
 
         return $this->render(array(
+            // ビューファイルで変数の存在をチェックするより
+            // アクション側で空の変数を指定するほうがビューファイルの見通しがよくなる
             'user_name' => '',
             'password'  => '',
             '_token'    => $this->generateCsrfToken('account/signup'),
         ));
     }
 
-//    public function registerAction()
-//    {
+    public function registerAction()
+    {
 //        if ($this->session->isAuthenticated()) {
 //            return $this->redirect('/account');
 //        }
-//
-//        if (!$this->request->isPost()) {
-//            $this->forward404();
-//        }
-//
-//        $token = $this->request->getPost('_token');
-//        if (!$this->checkCsrfToken('account/signup', $token)) {
-//            return $this->redirect('/account/signup');
-//        }
-//
-//        $user_name = $this->request->getPost('user_name');
-//        $password = $this->request->getPost('password');
-//
-//        $errors = array();
-//
-//        if (!strlen($user_name)) {
-//            $errors[] = 'ユーザIDを入力してください';
-//        } else if (!preg_match('/^\w{3,20}$/', $user_name)) {
-//            $errors[] = 'ユーザIDは半角英数字およびアンダースコアを3 ～ 20 文字以内で入力してください';
-//        } else if (!$this->db_manager->get('User')->isUniqueUserName($user_name)) {
-//            $errors[] = 'ユーザIDは既に使用されています';
-//        }
-//
-//        if (!strlen($password)) {
-//            $errors[] = 'パスワードを入力してください';
-//        } else if (4 > strlen($password) || strlen($password) > 30) {
-//            $errors[] = 'パスワードは4 ～ 30 文字以内で入力してください';
-//        }
-//
-//        if (count($errors) === 0) {
-//            $this->db_manager->get('User')->insert($user_name, $password);
-//            $this->session->setAuthenticated(true);
-//
-//            $user = $this->db_manager->get('User')->fetchByUserName($user_name);
-//            $this->session->set('user', $user);
-//
-//            return $this->redirect('/');
-//        }
-//
-//        return $this->render(array(
-//            'user_name' => $user_name,
-//            'password'  => $password,
-//            'errors'    => $errors,
-//            '_token'    => $this->generateCsrfToken('account/signup'),
-//        ), 'signup');
-//    }
-//
+
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('account/signup', $token)) {
+            return $this->redirect('/account/signup');
+        }
+
+        $user_name = $this->request->getPost('user_name');
+        $password = $this->request->getPost('password');
+
+        $errors = array();
+
+        if (!strlen($user_name)) {
+            $errors[] = 'ユーザIDを入力してください';
+        } else if (!preg_match('/^\w{3,20}$/', $user_name)) {  // \w は半角英数字とアンダースコア
+            $errors[] = 'ユーザIDは半角英数字およびアンダースコアを3 ～ 20 文字以内で入力してください';
+        } else if (!$this->db_manager->get('User')->isUniqueUserName($user_name)) {
+            $errors[] = 'ユーザIDは既に使用されています';
+        }
+
+        if (!strlen($password)) {
+            $errors[] = 'パスワードを入力してください';
+        } else if (4 > strlen($password) || strlen($password) > 30) {
+            $errors[] = 'パスワードは4 ～ 30 文字以内で入力してください';
+        }
+
+        if (count($errors) === 0) {
+            $this->db_manager->get('User')->insert($user_name, $password);
+            $this->session->setAuthenticated(true);
+
+            $user = $this->db_manager->get('User')->fetchByUserName($user_name);
+            $this->session->set('user', $user);
+
+            return $this->redirect('/');
+        }
+
+        return $this->render(array(
+            'user_name' => $user_name,
+            'password'  => $password,
+            'errors'    => $errors,
+            '_token'    => $this->generateCsrfToken('account/signup'),
+        ), 'signup'); // render メソッドの第2引数にテンプレートの名前を指定できる
+    }
+
 //    public function indexAction()
 //    {
 //        $user = $this->session->get('user');
