508c4455e9210c22460b8324ebff15362f88f478
INOUE Takuya <inouetakuya5@gmail.com>
2012-01-10 14:02:14 +0900

    P267  MiniBlogApplication クラス
    new file: MiniBlogApplication.php

---
+++ MiniBlogApplication.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * MiniBlogApplication.
+ *
+ * @author Katsuhiro Ogawa <fivestar@nequal.jp>
+ */
+class MiniBlogApplication extends Application
+{
+    protected $login_action = array('account', 'signin');
+
+    public function getRootDir()
+    {
+        return dirname(__FILE__);
+    }
+
+    protected function registerRoutes()
+    {
+        return array(
+        );
+    }
+
+    protected function configure()
+    {
+        $this->db_manager->connect('master', array(
+            'dsn'      => 'mysql:dbname=mini_blog;host=localhost',
+            'user'     => 'mini_blog_admin',
+            'password' => 'mini_blog_admin',
+        ));
+    }
+}
